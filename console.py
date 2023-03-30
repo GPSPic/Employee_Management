d2204cc335d50b626928f38e322ef58e2412eac4
GPSPic <picard.guillaume47@gmail.com>
2023-03-30 07:35:45 +0100

    remove commented out attempts

--- console.py
+++ console.py
@@ -12,38 +12,38 @@ manager1 = manager_repository.select(1)
 evaluation1 = Evaluation(99, "2022-11-01", "It's good to be evil", employee1, manager1)
 evaluation_repository.save(evaluation1)
 
-# evaluation1 = Evaluation(100, "2022-11-01", "blabla", employee1, manager1)
-# evaluation_repository.update(evaluation1)
+evaluation1 = Evaluation(100, "2022-11-01", "blabla", employee1, manager1)
+evaluation_repository.update(evaluation1)
 
-# pdb.set_trace()
-# manager1 = Manager("D'Artagnan", "picture_placeholer", "2022-12-01")
-# manager_repository.save(manager1)
+pdb.set_trace()
+manager1 = Manager("D'Artagnan", "picture_placeholer", "2022-12-01")
+manager_repository.save(manager1)
 
-# manager_list = manager_repository.select_all()
-# for manager in manager_list:
-#     print(manager.active)
+manager_list = manager_repository.select_all()
+for manager in manager_list:
+    print(manager.active)
 
-# manager_select_id1 = manager_repository.select(manager1.id)
-# print(manager_select_id1.end_date)
+manager_select_id1 = manager_repository.select(manager1.id)
+print(manager_select_id1.end_date)
 
-# manager1.end_date = "2012-12-12"
-# manager1.toggle_active()
-# manager_repository.update(manager1)
-# selected_manager = manager_repository.select(manager1.id)
-# print(selected_manager.end_date)  
+manager1.end_date = "2012-12-12"
+manager1.toggle_active()
+manager_repository.update(manager1)
+selected_manager = manager_repository.select(manager1.id)
+print(selected_manager.end_date)  
 
-# manager_list = manager_repository.select_all()
-# for manager in manager_list:
-#     print(manager.active)
+manager_list = manager_repository.select_all()
+for manager in manager_list:
+    print(manager.active)
 
-# employee1 = Employee("Tommy Oliver", "picture_placeholder", "Earth Protector", "5559999", "2021-11-11")
-# employee_repository.save(employee1)
+employee1 = Employee("Tommy Oliver", "picture_placeholder", "Earth Protector", "5559999", "2021-11-11")
+employee_repository.save(employee1)
 
-# employee_list = employee_repository.select_all()
-# for employee in employee_list:
-#     print(employee.qol_accommodations)
+employee_list = employee_repository.select_all()
+for employee in employee_list:
+    print(employee.qol_accommodations)
 
-# selected_employee = employee_repository.select_single_employee(5)
-# print(selected_employee.name)
-# print(selected_employee.active)
+selected_employee = employee_repository.select_single_employee(5)
+print(selected_employee.name)
+print(selected_employee.active)
 
--- controllers/employees_controller.py
+++ controllers/employees_controller.py
@@ -6,7 +6,6 @@ import repositories.employee_repository as employee_repository
 import repositories.manager_repository as manager_repository
 import repositories.evaluation_repository as evaluation_repository
 
-# from models.manager import Manager
 from models.employee import Employee
 
 employees_blueprint = Blueprint("employees", __name__)
